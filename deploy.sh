c01baf8a3498b68aee76cd96d231c0f3b9c253b1
Michael Lesniak <mail@mlesniak.com>
2020-09-04 06:26:27 +0200

    Use GitHub's container registry

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 docker build -t server .
-docker tag server 116.203.24.33:5000/markdown:latest
-docker push 116.203.24.33:5000/markdown:latest
-ssh server docker pull 116.203.24.33:5000/markdown:latest
+docker tag server ghcr.io/mlesniak/markdown:latest
+docker push ghcr.io/mlesniak/markdown:latest
+
+ssh server docker pull ghcr.io/mlesniak/markdown:latest
 ssh server docker rm --force markdown
-ssh server docker run -d --name markdown --env-file markdown.env -v /root/download:/data/download -it -p 8088:8080 116.203.24.33:5000/markdown:latest
+ssh server docker run -d --name markdown --env-file markdown.env -v /root/download:/data/download -it -p 8088:8080 ghcr.io/mlesniak/markdown:latest
 
 #cat <<EOF
 #docker pull 116.203.24.33:5000/markdown:latest
