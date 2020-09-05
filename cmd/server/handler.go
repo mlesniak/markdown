725266741ff02609da0737a0944c52236440f1f5
Michael Lesniak <mail@mlesniak.com>
2020-09-05 18:57:46 +0200

    Add cache functionality without clearing

--- cmd/server/handler.go
+++ cmd/server/handler.go
@@ -8,7 +8,17 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
+
+// Move this to own service including dropbox callback etc.
+type cacheEntry struct {
+	name      string
+	createdAt time.Time
+	data      []byte
+}
+
+var cache = make(map[string]cacheEntry)
 
 // handle is the default handler for all non-static content. It uses the parameter name
 // to download the correct markdown file from dropbox, perform various transformations
@@ -31,6 +41,15 @@ func handle(c echo.Context) error {
 
 	// Append markdown suffix and handle / - path.
 	filename = fixFilename(filename)
+
+	// Check if the file is in cache.
+	entry, ok := cache[filename]
+	if ok {
+		// Return generated HTML file with correct content type.
+		log.Infof("Using cache, TTL=todo. filename=%s", filename)
+		c.Response().Header().Add("Content-Type", "text/html; charset=UTF-8")
+		return c.String(http.StatusOK, string(entry.data))
+	}
 
 	// Read file from dropbox storage.
 	bs, err := dropboxService.Read(c.Logger(), filename)
@@ -64,6 +83,13 @@ func handle(c echo.Context) error {
 	}
 	html = strings.ReplaceAll(string(bsTemplate), "${content}", html)
 	html = strings.ReplaceAll(html, "${title}", titleLine)
+
+	// Add to cache.
+	cache[filename] = cacheEntry{
+		name:      filename,
+		createdAt: time.Now(),
+		data:      []byte(html),
+	}
 
 	// Return generated HTML file with correct content type.
 	c.Response().Header().Add("Content-Type", "text/html; charset=UTF-8")
