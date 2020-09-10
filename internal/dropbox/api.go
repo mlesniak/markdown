8c6072b5b69ed8d265138f37054359cd369b12f9
Michael Lesniak <mail@mlesniak.com>
2020-09-10 06:42:15 +0200

    Refactoring

---
+++ internal/dropbox/api.go
@@ -0,0 +1,93 @@
+package dropbox
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"io/ioutil"
+	"net/http"
+)
+
+// apiCallHeader generalizes different api calls to dropbox.
+//
+// Will later be non-public again after Refactoring.
+func (s *Service) apiCallHeader(log echo.Logger, url string, argument interface{}) ([]byte, error) {
+	// Create general request.
+	client := http.Client{}
+	request, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create request: %s", err)
+	}
+
+	// Create payload.
+	rawJson, err := json.Marshal(argument)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create payload: %s", err)
+	}
+
+	// Set token and payload for submitting.
+	request.Header.Add("Authorization", "Bearer "+s.token)
+	request.Header.Add("Dropbox-API-Arg", string(rawJson))
+
+	// Execute request.
+	log.Infof("Performing dropbox API call to %s with payload=%v", url, argument)
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("unable to perform request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	// Read response.
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read data from response: %s", err)
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("non 200 response from dropbox: `%s`", string(bs))
+	}
+
+	// Return data and log elapsed time.
+	return bs, err
+}
+
+// Consistency is not dropbox's strength, although I understand the idea behind this :-/
+func (s *Service) apiCall(log echo.Logger, url string, argument interface{}) ([]byte, error) {
+	// Create payload.
+	rawJson, err := json.Marshal(argument)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create payload: %s", err)
+	}
+	reader := bytes.NewReader(rawJson)
+
+	// Create general request.
+	client := http.Client{}
+	request, err := http.NewRequest("POST", url, reader)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create request: %s", err)
+	}
+
+	// Set token and payload for submitting.
+	request.Header.Add("Authorization", "Bearer "+s.token)
+	request.Header.Set("Content-Type", "application/json")
+
+	// Execute request.
+	log.Infof("Performing dropbox API call to %s with payload=%v", url, string(rawJson))
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("unable to perform request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	// Read response.
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read data from response: %s", err)
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("non 200 response from dropbox: `%s`", string(bs))
+	}
+
+	// Return data and log elapsed time.
+	return bs, err
+}
--- internal/dropbox/dropbox.go
+++ internal/dropbox/dropbox.go
@@ -1,12 +1,10 @@
 package dropbox
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"io/ioutil"
 	"net/http"
@@ -64,95 +62,12 @@ func (s *Service) Read(log echo.Logger, filename string) ([]byte, error) {
 	}{
 		Path: "/" + s.rootDirectory + filename,
 	}
-	bs, err := s.ApiCallHeader(log, "https://content.dropboxapi.com/2/files/download", argument)
+	bs, err := s.apiCallHeader(log, "https://content.dropboxapi.com/2/files/download", argument)
 	if err != nil {
 		return nil, err
 	}
 
 	log.Infof("Read file from dropbox. filename=%s, duration=%v", filename, time.Since(start).Milliseconds())
-	return bs, err
-}
-
-// ApiCallHeader generalizes different api calls to dropbox.
-//
-// Will later be non-public again after Refactoring.
-func (s *Service) ApiCallHeader(log echo.Logger, url string, argument interface{}) ([]byte, error) {
-	// Create general request.
-	client := http.Client{}
-	request, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create request: %s", err)
-	}
-
-	// Create payload.
-	rawJson, err := json.Marshal(argument)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create payload: %s", err)
-	}
-
-	// Set token and payload for submitting.
-	request.Header.Add("Authorization", "Bearer "+s.token)
-	request.Header.Add("Dropbox-API-Arg", string(rawJson))
-
-	// Execute request.
-	log.Infof("Performing dropbox API call to %s with payload=%v", url, argument)
-	resp, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("unable to perform request: %s", err)
-	}
-	defer resp.Body.Close()
-
-	// Read response.
-	bs, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("unable to read data from response: %s", err)
-	}
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("non 200 response from dropbox: `%s`", string(bs))
-	}
-
-	// Return data and log elapsed time.
-	return bs, err
-}
-
-// Consistency is not dropbox's strength, although I understand the idea behind this :-/
-func (s *Service) apiCall(log echo.Logger, url string, argument interface{}) ([]byte, error) {
-	// Create payload.
-	rawJson, err := json.Marshal(argument)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create payload: %s", err)
-	}
-	reader := bytes.NewReader(rawJson)
-
-	// Create general request.
-	client := http.Client{}
-	request, err := http.NewRequest("POST", url, reader)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create request: %s", err)
-	}
-
-	// Set token and payload for submitting.
-	request.Header.Add("Authorization", "Bearer "+s.token)
-	request.Header.Set("Content-Type", "application/json")
-
-	// Execute request.
-	log.Infof("Performing dropbox API call to %s with payload=%v", url, string(rawJson))
-	resp, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("unable to perform request: %s", err)
-	}
-	defer resp.Body.Close()
-
-	// Read response.
-	bs, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("unable to read data from response: %s", err)
-	}
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("non 200 response from dropbox: `%s`", string(bs))
-	}
-
-	// Return data and log elapsed time.
 	return bs, err
 }
 
